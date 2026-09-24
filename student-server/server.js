21c987f4b48b02b3623f95e799d9d410e81244da
Minlish-Git <e22303@eng.pdn.ac.lk>
2026-09-25 04:52:57 +0530

    Update server.js

--- student-server/server.js
+++ student-server/server.js
@@ -20,10 +20,10 @@ app.use(express.json()); // Allows sending JSON data
 
 // 1. Create a Pool instead of a single connection
 const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "1234",
-  database: "school_lms",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   port: process.env.DB_PORT || 3306,
   waitForConnections: true,
   connectionLimit: 10,
